04e0c232c887da4c1b4362db6c336edd1358d881
Voroshilov Maksim <voroshilovmax90@gmail.com>
2026-02-24 16:32:11 +0100

    added test and deploy script

--- test/ERC20Bridge.t.sol
+++ test/ERC20Bridge.t.sol
@@ -6,7 +6,6 @@ import "../src/ERC20Bridge.sol";
 import "./MockERC20.sol";
 
 contract ERC20BridgeTest is Test {
-
     ERC20Bridge bridge;
     MockERC20 token;
 
@@ -22,15 +21,11 @@ contract ERC20BridgeTest is Test {
     address evmRecipient = address(0x200);
 
     function setUp() public {
-
         relayer1 = vm.addr(relayer1Pk);
         relayer2 = vm.addr(relayer2Pk);
         relayer3 = vm.addr(relayer3Pk);
 
-        bridge = new ERC20Bridge(
-            2,              // threshold
-            address(this)   // owner
-        );
+        bridge = new ERC20Bridge(2);
 
         bridge.addRelayer(relayer1);
         bridge.addRelayer(relayer2);
@@ -52,14 +47,9 @@ contract ERC20BridgeTest is Test {
     // -----------------------------
 
     function testLock() public {
-
         vm.prank(user);
 
-        bridge.lock(
-            address(token),
-            100 ether,
-            "cosmos1abc..."
-        );
+        bridge.lock(address(token), 100 ether, "cosmos1abc...");
 
         assertEq(token.balanceOf(address(bridge)), 100 ether);
     }
@@ -69,33 +59,28 @@ contract ERC20BridgeTest is Test {
     // -----------------------------
 
     function testUnlockSuccess() public {
+        ERC20Bridge.Claim memory claim = ERC20Bridge.Claim({
+            evmChainId: block.chainid,
+            token: address(token),
+            to: evmRecipient,
+            amount: 50 ether,
+            nonce: 1
+        });
 
-        ERC20Bridge.Claim memory claim =
-            ERC20Bridge.Claim({
-                evmChainId: block.chainid,
-                token: address(token),
-                to: evmRecipient,
-                amount: 50 ether,
-                nonce: 1
-            });
+        token.mint(address(bridge), 50 ether);
 
         bytes32 hash = bridge.getMessageHash(claim);
         bytes32 ethSignedHash = keccak256(
             abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
         );
 
-        (uint8 v1, bytes32 r1, bytes32 s1) =
-            vm.sign(relayer1Pk, ethSignedHash);
+        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(relayer1Pk, ethSignedHash);
 
-        (uint8 v2, bytes32 r2, bytes32 s2) =
-            vm.sign(relayer2Pk, ethSignedHash);
+        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(relayer2Pk, ethSignedHash);
 
-        bytes;
-
+        bytes[] memory sigs = new bytes[](2);
         sigs[0] = abi.encodePacked(r1, s1, v1);
         sigs[1] = abi.encodePacked(r2, s2, v2);
-
-        token.mint(address(bridge), 50 ether);
 
         bridge.unlock(claim, sigs);
 
@@ -107,30 +92,27 @@ contract ERC20BridgeTest is Test {
     // -----------------------------
 
     function testUnlockFailNotEnoughSignatures() public {
-
-        ERC20Bridge.Claim memory claim =
-            ERC20Bridge.Claim({
-                evmChainId: block.chainid,
-                token: address(token),
-                to: evmRecipient,
-                amount: 50 ether,
-                nonce: 2
-            });
+        ERC20Bridge.Claim memory claim = ERC20Bridge.Claim({
+            evmChainId: block.chainid,
+            token: address(token),
+            to: evmRecipient,
+            amount: 50 ether,
+            nonce: 2
+        });
 
         bytes32 hash = bridge.getMessageHash(claim);
         bytes32 ethSignedHash = keccak256(
             abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
         );
 
-        (uint8 v1, bytes32 r1, bytes32 s1) =
-            vm.sign(relayer1Pk, ethSignedHash);
+        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(relayer1Pk, ethSignedHash);
 
-        bytes;
+        bytes[] memory sigs = new bytes[](1);
         sigs[0] = abi.encodePacked(r1, s1, v1);
 
         token.mint(address(bridge), 50 ether);
 
-        vm.expectRevert("not enough signatures");
+        vm.expectRevert();
 
         bridge.unlock(claim, sigs);
     }
@@ -140,29 +122,24 @@ contract ERC20BridgeTest is Test {
     // -----------------------------
 
     function testUnlockReplayFails() public {
-
-        ERC20Bridge.Claim memory claim =
-            ERC20Bridge.Claim({
-                evmChainId: block.chainid,
-                token: address(token),
-                to: evmRecipient,
-                amount: 50 ether,
-                nonce: 3
-            });
+        ERC20Bridge.Claim memory claim = ERC20Bridge.Claim({
+            evmChainId: block.chainid,
+            token: address(token),
+            to: evmRecipient,
+            amount: 50 ether,
+            nonce: 3
+        });
 
         bytes32 hash = bridge.getMessageHash(claim);
         bytes32 ethSignedHash = keccak256(
             abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
         );
 
-        (uint8 v1, bytes32 r1, bytes32 s1) =
-            vm.sign(relayer1Pk, ethSignedHash);
+        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(relayer1Pk, ethSignedHash);
 
-        (uint8 v2, bytes32 r2, bytes32 s2) =
-            vm.sign(relayer2Pk, ethSignedHash);
+        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(relayer2Pk, ethSignedHash);
 
-        bytes;
-
+        bytes[] memory sigs = new bytes[](2);
         sigs[0] = abi.encodePacked(r1, s1, v1);
         sigs[1] = abi.encodePacked(r2, s2, v2);
 
